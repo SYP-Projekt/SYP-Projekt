b88f7a4b172b30f8c0cdcdc3638b9967b1247584
Stefan Rautner <115265677+StefanRautner@users.noreply.github.com>
2023-03-08 11:39:03 +0100

    Update Database_SYP-Projekt.sql

--- Database/Database_SYP-Projekt.sql
+++ Database/Database_SYP-Projekt.sql
@@ -2,21 +2,28 @@ CREATE DATABASE IF NOT EXISTS speicher_DB;
 USE speicher_DB;
 
 CREATE TABLE IF NOT EXISTS Player (
-`spielerID` INT(4) NOT NULL AUTO_INCREMENT,
+`spielerID` INT(1) NOT NULL AUTO_INCREMENT,
+`inventoryID` INT(1) NOT NULL,
 `xkoord` FLOAT(8) NOT NULL,
 `ykoord` FLOAT(8) NOT NULL,
-#z
-PRIMARY KEY (spielerID);
+`zkoord` FLOAT(8) NOT NULL,
+PRIMARY KEY(spielerID),
+CONSTRAINT FK_inventoryID FOREIGN KEY (inventoryID) REFERENCES Inventory(inventoryID));
 
 CREATE TABLE IF NOT EXISTS Items (
 `ID` INT(4) NOT NULL AUTO_INCREMENT,
 `name` VARCHAR(20) NOT NULL,
-#Values of Items
-PRIMARY KEY (id));
+PRIMARY KEY(id));
+
+CREATE TABLE IF NOT EXISTS ItemList (
+`itemListID` INT(1) NOT NULL AUTO_INCREMENT,
+`itemID` INT(2) NOT NULL,
+PRIMARY KEY(itemListID),
+CONSTRAINT FK_item FOREIGN KEY (itemID) REFERENCES Items(ID));
 
 CREATE TABLE IF NOT EXISTS Inventory (
-`inventoryID` INT(4) NOT NULL AUTO_INCREMENT,
-`itemID` INT(4) NOT NULL,
-#items nua zua listn hinzu ned zum inventar
-PRIMARY KEY (inventoryid),
-CONSTRAINT FK_item FOREIGN KEY (itemID) REFERENCES Items(ID));
+`inventoryID` INT(1) NOT NULL AUTO_INCREMENT,
+`itemListID` INT(1) NOT NULL,
+-- Liste, besteht aus items --
+PRIMARY KEY(inventoryid),
+CONSTRAINT FK_item FOREIGN KEY (itemListID) REFERENCES Items(ID));
