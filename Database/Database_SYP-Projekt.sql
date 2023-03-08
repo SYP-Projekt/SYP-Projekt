cab084e6c3b1dace6e653fdd5026a45f4339a6e2
HazeAT <tobias.haas@htl-saalfelden.at>
2023-03-08 09:18:27 +0100

    Datenbank_Überarbeitung

--- Database/Database_SYP-Projekt.sql
+++ Database/Database_SYP-Projekt.sql
@@ -3,21 +3,20 @@ USE speicher_DB;
 
 CREATE TABLE IF NOT EXISTS Player (
 `spielerID` INT(4) NOT NULL AUTO_INCREMENT,
-`inventoryID` INT(4) NOT NULL,
 `xkoord` FLOAT(8) NOT NULL,
 `ykoord` FLOAT(8) NOT NULL,
-PRIMARY KEY (spielerID),
-CONSTRAINT FK_inventoryID FOREIGN KEY (inventoryID) REFERENCES Inventory(inventoryID));
+#z
+PRIMARY KEY (spielerID);
 
 CREATE TABLE IF NOT EXISTS Items (
 `ID` INT(4) NOT NULL AUTO_INCREMENT,
 `name` VARCHAR(20) NOT NULL,
+#Values of Items
 PRIMARY KEY (id));
 
 CREATE TABLE IF NOT EXISTS Inventory (
 `inventoryID` INT(4) NOT NULL AUTO_INCREMENT,
 `itemID` INT(4) NOT NULL,
-`ykoord` FLOAT(8) NOT NULL,
-`xkoord` FLOAT(8) NOT NULL,
+#items nua zua listn hinzu ned zum inventar
 PRIMARY KEY (inventoryid),
 CONSTRAINT FK_item FOREIGN KEY (itemID) REFERENCES Items(ID));
